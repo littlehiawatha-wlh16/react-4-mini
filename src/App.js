8942aa6bd0fbd10762adf045d198095826b70ebc
Jacob Hall <halljaco@yahoo.com>
2020-02-13 12:02:31 -0700

    React-4 Lecture 5.4.mp end of class

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 import Nav from "./Components/Nav/Nav";
+import {HashRouter as Router} from 'react-router-dom'
+import routes from './routes'
 import "./App.css";
 
 class App extends Component {
   render() {
+    //console.log(this.props)
     return (
-      <div className="App">
-        <Nav />
-        <h1 style={{ padding: "200px 35%" }}>
-          This is where your pages will appear
-        </h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Nav />
+          {routes}
+        </div>
+      </Router>
     );
   }
 }
